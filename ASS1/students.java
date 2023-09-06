de9ed35d828315bdb03716bf4a5455864d249455
gkavishika21 <47886738+gkavishika21@users.noreply.github.com>
2023-09-06 09:52:14 +0800

    commit 2
    rectify the code issues.

--- ASS1/students.java
+++ ASS1/students.java
@@ -1,37 +1,34 @@
-
+import java.util.Scanner;
 /**
  * Write a description of class students here.
  *
  * @author (M.G.Gimhani Kavishika)
  * @Student ID (24097251)
  * @Date (3/9/2023)
+ * Assigment 1
  */
-
-import java.util.Scanner;
 
 public class students
 {
     // instance variables - replace the example below with your own
     String assigmentName; 
-    int marks;
-     
-
+    
 
         public static void main(String[] args){
+            double[] marks = new double[30];
             
             Scanner input = new Scanner(System.in);
-            
             System.out.println("Enter the your Assigment name: ");
+            assigmentName : input.nextLine();   
             
-            assigmentName : input.nextLine();
+           
             
-            for(int i=0;i < marks.length; i++) {
-                
-            System.out.println("Enter a valid mark : " );
-                marks = input.nextLine();
-            
-                if(marks >= 0 && marks <= 30) {
-                    return marks;                    
+            for(int i=0;i < marks.length; i++) {  
+                System.out.println("Enter a valid mark : " );
+                Scanner marksInput = new Scanner(System.in);
+                marks[i] = marksInput.nextDouble();
+                    if(marks[i] >=0 && marks[i] <= 30) {
+                                          
                 }
                 
                 else{
@@ -39,8 +36,29 @@ public class students
                 }
                 
             }
+            
+            //Find Highest mark from the marks list
+            double highestMark = marks[0];
+            
+            for(int j =0; j<10 ; j++) {
+           if(marks[j] > highestMark){
+               highestMark = marks[j];
+           }
         }
+        //Print the highestMark
+        
+        System.out.println("The Highest Mark is : " +highestMark);
+        
+        //Find Minium mark from the marks list
+        double miniumMark = marks[0];
+        
+        for (int i = 1; i < marks.length; i++) {
+            if (marks[i] < miniumMark) {
+                miniumMark = marks[i];
     }
+}
+}
+}
     
             
             
