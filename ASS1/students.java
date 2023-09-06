6355c5e23d38af0be63ff25827848e2563dc4d72
gkavishika21 <47886738+gkavishika21@users.noreply.github.com>
2023-09-06 15:35:07 +0800

    commit 3
    created mean,Sd and print marks and assigment name

--- ASS1/students.java
+++ ASS1/students.java
@@ -11,17 +11,16 @@ import java.util.Scanner;
 public class students
 {
     // instance variables - replace the example below with your own
-    String assigmentName; 
+     
     
 
         public static void main(String[] args){
+            //String assigmentName;
             double[] marks = new double[30];
             
             Scanner input = new Scanner(System.in);
             System.out.println("Enter the your Assigment name: ");
-            assigmentName : input.nextLine();   
-            
-           
+            String assigmentName = input.nextLine();   
             
             for(int i=0;i < marks.length; i++) {  
                 System.out.println("Enter a valid mark : " );
@@ -34,15 +33,20 @@ public class students
                 else{
                     System.out.println("Invalid mark. Please Enter mark between 0 and 30.");
                 }
-                
             }
+            //Print the assigment name
+            System.out.println("\nAssignment: " +assigmentName);
             
+            //Print the assigment Marks
+             for (int i = 0; i < marks.length; i++) {
+            System.out.println("Student " + (i + 1) + ": " + marks[i]);
+            }
             //Find Highest mark from the marks list
             double highestMark = marks[0];
             
-            for(int j =0; j<10 ; j++) {
-           if(marks[j] > highestMark){
-               highestMark = marks[j];
+            for(int j =0; j<marks.length; j++) {
+                if(marks[j] > highestMark){
+                    highestMark = marks[j];
            }
         }
         //Print the highestMark
@@ -55,9 +59,35 @@ public class students
         for (int i = 1; i < marks.length; i++) {
             if (marks[i] < miniumMark) {
                 miniumMark = marks[i];
+            }
+        }
+        //Print the Minimum mark from the marks list
+        System.out.println("The Minimum Mark is : " +miniumMark);
+        
+        //Find Mean of marks from the marks list
+        double sum = 0;
+        double mean;
+        
+        for (double mark : marks){
+            sum += mark;
+        }
+        mean = sum / marks.length;
+        //Print the Mean of marks from the marks list
+        System.out.println("The Mean mark is : "+mean );
+        
+        //Find Stand Deviation of the marks from the marks list
+        double difference = 0;
+        double sumOfDifferences = 0;
+        double variance;
+        
+        for(double mark : marks){
+            difference = mark - mean; 
+            sumOfDifferences += difference * difference;
+        }
+        variance = sumOfDifferences/marks.length;
+        //Print the Stand Deviation of marks 
+        System.out.println("The Stand Deviation mark is : "+variance);
     }
-}
-}
 }
     
             
