2691e721149e35a686da3e6b6799e7d70c69661b
gkavishika21 <47886738+gkavishika21@users.noreply.github.com>
2023-09-09 23:14:17 +0800

    updated

--- ASS1/students.java
+++ ASS1/students.java
@@ -12,9 +12,6 @@ import java.util.Scanner;
 public class students
 {
         public static void main(String[] args){
-            //String assigmentName;
-            //double[] marks = new double[4];
-            //double[] validMarks = new double[4];
             Scanner input = new Scanner(System.in);
             
             //Input the assigment name
@@ -22,30 +19,17 @@ public class students
             String assigmentName = input.nextLine();   
             
             // Input student's marks
-            
-            double[] assigmentMarks = new double[30];
+            double[] assigmentMarks = new double[10];
             for(int i=0;i < assigmentMarks.length; i++) { 
                 System.out.println("Assigment Mark " + (i+1));
+                
               //check validation of the input marks
                 double assigmentMark = getValidMarks(input);
                 assigmentMarks[i] = assigmentMark;
             }
                 
-                //System.out.println("Enter a valid mark : " );
-                //Scanner marksInput = new Scanner(System.in);
-                //marks[i] = marksInput.nextDouble();
-                    //if(marks[i] >=0 && marks[i] <= 30) {
-                        
-                            //validMarks[i] = marks[i]; 
-           //}
-                //else{
-                    //System.out.println("Invalid mark. Please Enter mark between 0 and 30.");
-                //}
-            
-        //}
             //Print the assigment name
             System.out.println("\nAssignment: " +assigmentName);
-        
         
         // Call functions 
         double highestMark = findHighestMark(assigmentMarks);
@@ -62,63 +46,10 @@ public class students
         System.out.println("The Mean mark is : "+mean );
         System.out.println("The Stand Deviation mark is : "+stanardDeviation);
         
-    
-            //Print the assigment Marks
-        /**for (int i = 0; i < marks.length; i++) {
-            if(validMarks[i] != 0.00 ){
-                  System.out.println("Student " + (i + 1) + ": " + validMarks[i]);
-                 }
-            }
-            //Find Highest mark from the marks list
-           double highestMark = marks[0];
-            
-           for(int j =0; j<marks.length; j++) {
-              if(marks[j] > highestMark){
-                   highestMark = marks[j];
-           }
-        }
-        //Print the highestMark
-        
-        System.out.println("The Highest Mark is : " +highestMark);
-        
-        //Find Minium mark from the marks list
-        double miniumMark = marks[0];
-        
-        for (int i = 1; i < marks.length; i++) {
-            if (marks[i] < miniumMark) {
-                miniumMark = marks[i];
-            }*/
-        //}
-        //Print the Minimum mark from the marks list
-       // System.out.println("The Minimum Mark is : " +miniumMark);
-        
-        //Find Mean of marks from the marks list
-       // double sum =0;
-       // double mean;
-        
-       // for (double mark : marks){
-          //  sum += mark;
-        //}
-       // mean = sum / marks.length;
-        //Print the Mean of marks from the marks list
-       // System.out.println("The Mean mark is : "+mean );
-        
-        //Find Stand Deviation of the marks from the marks list
-      //  double difference = 0;
-      //  double sumOfDifferences = 0;
-      //  double variance;
-        
-        //for(double mark : marks){
-          //  difference = mark - mean; 
-          //  sumOfDifferences += difference * difference;
-      //  }
-        //variance = sumOfDifferences/marks.length;
-        //Print the Stand Deviation of marks 
-     //   System.out.println("The Stand Deviation mark is : "+variance);
-  //}
+    }     
     
     //Function no 1 to get valid marks from the user
-}
+
     public static double getValidMarks(Scanner input){
       while (true){
            System.out.println("Please Enter a Valid marks between 0-30 : ");
@@ -174,7 +105,7 @@ public class students
             sumOfDifferences += difference * difference; 
         }
         variance = sumOfDifferences / assigmentMarks.length;
-        return variance;
+        return Math.sqrt(variance);
     }
 }
 
